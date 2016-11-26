03aa9b0dd07b8d1423f538327f6ff928a9a5c244
Sampath Surineni <sampath.surineni@oracle.com>
2016-11-26 19:45:06 +0530

    put prelude in seperate file

--- .emacs
+++ .emacs
@@ -1,4 +1,11 @@
 ;; Start emacs server
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (server-start)
 ;; Disable gui parts
 (menu-bar-mode -1)
@@ -8,6 +15,10 @@
 (setq-default cursor-type 'bar)
 ;; saves opened files on exit
 (desktop-save-mode 1)
+;; no need to type full yes
+(fset 'yes-or-no-p 'y-or-n-p)
+;; wont ask if creating a new buffer
+(setq confirm-nonexistent-file-or-buffer nil)
 
 ;; key binding where no package is required
 (global-set-key (kbd "RET") 'newline-and-indent);return will indent now
@@ -16,3 +27,6 @@
 (global-set-key (kbd "C-\\") 'split-window-right)
 (global-set-key (kbd "C-a") 'back-to-indentation)
 (global-set-key (kbd "C-b") 'backward-word)
+
+;; installing packages
+(load-file "~/elisp/prelude.el")
---
+++ elisp/prelude.el
@@ -0,0 +1,36 @@
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+            '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+            '("elpa" . "http://elpa.gnu.org/packages/") t)
+(defvar prelude-packages
+  '(aggressive-indent auto-complete auto-package-update
+		      color-theme-sanityinc-tomorrow hungry-delete
+  		      diminish fill-column-indicator org web-mode
+		      projectile epc ido-vertical-mode
+		      company js2-mode js2-refactor ac-js2 tern transpose-frame elpy
+		      flx-ido magit beacon dash 
+		      dash-functional
+		      keyfreq groovy-mode smartparens)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (cl-loop for p in prelude-packages
+	   when (not (package-installed-p p)) do (cl-return nil)
+	   finally (cl-return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
+;; prelude end
