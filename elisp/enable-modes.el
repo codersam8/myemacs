3da1b143881a74abf544ca9fc3b444c07188929f
sampath <sampath.surineni@oracle.com>
2017-05-14 15:40:59 +0530

    required fci-mode

--- elisp/enable-modes.el
+++ elisp/enable-modes.el
@@ -10,6 +10,19 @@
   "No maybe for you. Only AC!"
   (unless (minibufferp (current-buffer))
     (auto-complete-mode 1)))
+;; workaround for fci-mode
+(require 'fill-column-indicator)
+(defvar sanityinc/fci-mode-suppressed nil)
+(defadvice popup-create (before suppress-fci-mode activate)
+  "Suspend fci-mode while popups are visible"
+  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+  (when fci-mode
+    (turn-off-fci-mode)))
+(defadvice popup-delete (after restore-fci-mode activate)
+  "Restore fci-mode when all popups have closed"
+  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+    (setq sanityinc/fci-mode-suppressed nil)
+    (turn-on-fci-mode)))
 ;; avy-mode
 (global-set-key (kbd "C-;") 'avy-goto-char-timer)
 ;; ido mode
