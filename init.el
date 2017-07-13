285ebe6edd5b4dc6766fe8114b40b56e0ba28b40
sampath surineni <sampath.surineni@oracle.com>
2017-07-13 11:23:40 +0100

    updated with master

--- init.el
+++ init.el
@@ -77,10 +77,10 @@
 
 (setq grep-program "\"D:/software/installed/unixutils/UnxUpdates/grep.exe\"")
 ;; defining functions
-(load-file "~/.emacs.d/elisp/functions.el")
+(load-file "~/myemacs/elisp/functions.el")
 ;; installing packages
-(load-file "~/.emacs.d/elisp/prelude.el")
-(load-file "~/.emacs.d/elisp/enable-modes.el")
+(load-file "~/myemacs/elisp/prelude.el")
+(load-file "~/myemacs/elisp/enable-modes.el")
 ;; theme path
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
