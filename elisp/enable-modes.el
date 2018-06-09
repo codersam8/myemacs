91178aaefca0bf3f4424a66290ec17e32e5c76c5
sampath <sampath.skt@gmail.com>
2018-06-09 12:26:49 -0700

    commented about fci mode

--- elisp/enable-modes.el
+++ elisp/enable-modes.el
@@ -31,17 +31,17 @@
 (pyvenv-activate "~/pyython")
 
 ;; workaround for fci-mode
-(defvar sanityinc/fci-mode-suppressed nil)
-(defadvice popup-create (before suppress-fci-mode activate)
-  "Suspend fci-mode while popups are visible"
-  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
-  (when fci-mode
-    (turn-off-fci-mode)))
-(defadvice popup-delete (after restore-fci-mode activate)
-  "Restore fci-mode when all popups have closed"
-  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
-    (setq sanityinc/fci-mode-suppressed nil)
-    (turn-on-fci-mode)))
+;; (defvar sanityinc/fci-mode-suppressed nil)
+;; (defadvice popup-create (before suppress-fci-mode activate)
+;;   "Suspend fci-mode while popups are visible"
+;;   (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+;;   (when fci-mode
+;;     (turn-off-fci-mode)))
+;; (defadvice popup-delete (after restore-fci-mode activate)
+;;   "Restore fci-mode when all popups have closed"
+;;   (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+;;     (setq sanityinc/fci-mode-suppressed nil)
+;;     (turn-on-fci-mode)))
 ;; avy-mode
 (global-set-key (kbd "C-;") 'avy-goto-char-timer)
 
